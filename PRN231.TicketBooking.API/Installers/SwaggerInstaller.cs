0c7ed5b94c0f0a770f06b3469b82041de79a3b4d
QuanDH <hongquan.contact@gmail.com>
2024-05-11 09:40:31 +0700

    change name and port api

--- PRN231.TicketBooking.API/Installers/SwaggerInstaller.cs
+++ PRN231.TicketBooking.API/Installers/SwaggerInstaller.cs
@@ -34,8 +34,8 @@ namespace PRN231.TicketBooking.API.Installers
                  option.SwaggerDoc("v1", new OpenApiInfo
                  {
                      Version = "v1",
-                     Title = "House construction quotation API",
-                     Description = "©Copyright: Hồng Quân & Minh Khang",
+                     Title = "Event Organization API",
+                     Description = "©Copyright: Hồng Quân & Minh Khang & Xuân Tiến & Bình An",
                      TermsOfService = new Uri("https://example.com/terms"),
                      Contact = new OpenApiContact
                      {
