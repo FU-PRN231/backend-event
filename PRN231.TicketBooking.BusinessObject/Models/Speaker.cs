2fdcf21ae5ec8109a0a0ccdfd3f3d8b63d5881b4
KhangPBM <phamkhang12378@gmail.com>
2024-05-12 10:38:11 +0700

    Add Speaker table and

--- PRN231.TicketBooking.BusinessObject/Models/Speaker.cs
+++ PRN231.TicketBooking.BusinessObject/Models/Speaker.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
@@ -8,15 +7,14 @@ using System.Threading.Tasks;
 
 namespace PRN231.TicketBooking.BusinessObject.Models
 {
-    public class Sponsor
+    public class Speaker
     {
-        [Key]
         public Guid Id { get; set; }
         public string Name { get; set; } = null!;
         public string Description { get; set; } = null!;
         public string Img { get; set; } = null!;
-        public string AccountId { get; set; }
-        [ForeignKey(nameof(AccountId))]
-        public Account? Account {  get; set; } 
+        public Guid EventId { get; set; }
+        [ForeignKey(nameof(EventId))]
+        public Event? Event { get; set; }
     }
 }
