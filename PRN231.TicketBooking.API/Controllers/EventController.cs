c63379b1a7174fc3884c237e47c3892cb3431e41
Ga Honghac <flamingotran1532002@gmail.com>
2024-05-22 14:53:28 +0700

    create event controller

---
+++ PRN231.TicketBooking.API/Controllers/EventController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PRN231.TicketBooking.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventController : ControllerBase
+    {
+    }
+}
