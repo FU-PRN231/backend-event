7e58e0229f7acc1f9622f3e7d959cdb8e08a7089
GaHongHacDev <tientranxuan263@gmail.com>
2024-06-20 23:41:29 +0700

    code get all sponsor api

--- PRN231.TicketBooking.Repository/Implementation/OrganzationRepository.cs
+++ PRN231.TicketBooking.Repository/Implementation/OrganzationRepository.cs
@@ -1,4 +1,5 @@
 ﻿using PRN231.TicketBooking.BusinessObject.Models;
+using PRN231.TicketBooking.Common.Dto.Response;
 using PRN231.TicketBooking.DAO.dao;
 using PRN231.TicketBooking.Repository.Contract;
 using System;
@@ -11,8 +12,29 @@ namespace PRN231.TicketBooking.Repository.Implementation
 {
     public class OrganzationRepository : GenericRepository<Organization>, IOrganizationRepository
     {
+        private readonly IGenericDAO<Organization> _organizationDAO;
         public OrganzationRepository(IGenericDAO<Organization> dao, IServiceProvider serviceProvider) : base(dao, serviceProvider)
         {
+            _organizationDAO = dao;
+        }
+
+        public async Task<PagedResult<Organization>> GetOrganizations(int pageNumber, int pageSize)
+        {
+            PagedResult<Organization> result = null;
+            try
+            {
+                result = new PagedResult<Organization>();
+                result = await _organizationDAO.GetAllDataByExpression(
+                    filter: null,
+                    pageNumber: pageNumber,
+                    pageSize: pageSize
+                );
+            }
+            catch (Exception ex)
+            {
+                result = null;
+            }
+            return result;
         }
     }
 }
