4b662df636950422fc8d728fd01dcd19c69bceae
KhangPBM <phamkhang12378@gmail.com>
2024-06-27 15:07:45 +0700

    fix attachments

--- PRN231.TicketBooking.Service/Implementation/OrderService.cs
+++ PRN231.TicketBooking.Service/Implementation/OrderService.cs
@@ -573,7 +573,7 @@ namespace PRN231.TicketBooking.Service.Implementation
                         files.Add(img);
                     }
                 }
-                emailService!.SendEmailWithFiles(account.Email, SD.SubjectMail.SEAT_TICKET, body, ticketInfo);
+                emailService!.SendEmailWithFiles(account.Email, SD.SubjectMail.SEAT_TICKET, body, files);
             }
             catch (Exception ex)
             {
